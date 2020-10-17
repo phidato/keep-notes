05f354521ad5f2b8c297f6bda9915ed361ae60f2
phidato <rizahmed.in@gmail.com>
2020-10-17 12:18:31 +0530

    some design issue fixed

--- main_config/settings.py
+++ main_config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'notes',
 ]
 
@@ -123,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- notes/forms.py
+++ notes/forms.py
@@ -5,3 +5,6 @@ class BookCreateForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'rows':7, 'cols':15}),
+        }
